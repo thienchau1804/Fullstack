571d3f93df5a05ccda3c25be653113f51fd315d0
thienchau1804 <mschau1232@gmail.com>
2024-08-13 11:01:42 +0700

    add new file

---
+++ javascripts.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function dynamicDropdown(listIndex) {
+    document.getElementById("rooms").length = 0;
+
+    for (let i = 1; i < Number(listIndex) + 1; i++) {
+        document.getElementById("rooms").options[i - 1] = new Option(i.toString(), i);
+    }
+}
